fbbdfcddbefc1a05d653115547913fc0868895d0
otaviobarbosa617 <barbosaoapb@gmail.com>
2022-02-28 10:11:12 -0500

    employee class

---
+++ employee.py
@@ -0,0 +1,47 @@
+# Lab Professor: Ms. Laily Ajellu
+# COMP 2152 - Assignment 1
+# Otavio Pereira-Barbosa - 101337690
+
+class Employee:
+
+    def __init__(self, employee_id, employee_name, employee_type, employee_years, employee_purchased,
+                 employee_discounts, employee_discount_number):
+        self.employee_id = employee_id
+        self.employee_name = employee_name
+        self.employee_type = employee_type
+        self.employee_years = employee_years
+        self.employee_discount_number = employee_discount_number
+        self.employee_discounts = employee_discounts
+        self.employee_purchased = employee_purchased
+
+    def set_employee_id(self, employee_id):
+        self.employee_id = employee_id
+
+    def set_employee_name(self, employee_name):
+        self.employee_name = employee_name
+
+    def set_employee_type(self, employee_type):
+        self.employee_type = employee_type
+
+    def set_employee_years(self, employee_years):
+        self.employee_years = employee_years
+
+    def set_employee_purchased(self, employee_purchased):
+        self.employee_purchased = employee_purchased
+
+    def set_employee_discounts(self, employee_discounts):
+        self.employee_discounts = employee_discounts
+
+    def set_employee_discount_number(self, employee_discount_number):
+        self.employee_discount_number = employee_discount_number
+
+    def get_employee_id(self):
+        self.employee_id
+
+    def __str__(self) -> str:
+        return f"\nEmployee ID: {self.employee_id}\n" \
+               f"Employee Name: {self.employee_name}\n" \
+               f"Employee Type {self.employee_type}\n" \
+               f"Employee Years Worked: {self.employee_years}\n" \
+               f"Employee Total Discounts: {self.employee_discounts}\n" \
+               f"Employee Discount Number: {self.employee_discount_number}\n"
--- main.py
+++ main.py
@@ -2,16 +2,30 @@
 # COMP 2152 - Assignment 1
 # Otavio Pereira-Barbosa - 101337690
 
+import employee
+
+employee_list = []
+
+
 def menu():
-    user_choice = 0
-    while user_choice != 5:
+    menu_active = True
+    while menu_active:
+        print(35 * "-")
+        print("|\t\t\tMain Menu", "|".rjust(13))
+        print("| 1 - Create Employee", "|".rjust(13))
+        print("| 2 - Create Item", "|".rjust(17))
+        print("| 3 - Make Purchase", "|".rjust(15))
+        print("| 4 - All Employee Summary", "|".rjust(8))
+        print("| 5 - Exit", "|".rjust(24))
+        print(35 * "-")
         try:
-            user_choice = int(input("Enter a valid choice to select it:"))
+            user_choice = int(input("Enter a valid choice to select it: "))
         except UserWarning:
             print("You have not selected a valid menu item")
         else:
             if user_choice == 1:
-                create_employee()
+                create_employee(employee_list)
+                continue
             elif user_choice == 2:
                 create_item()
             elif user_choice == 3:
@@ -19,9 +33,30 @@ def menu():
             elif user_choice == 4:
                 summary()
             elif user_choice == 5:
-                break
+                print("")
+                print("\t\t\tBye Bye")
+                menu_active = False
             else:
                 print("Please enter a valid option")
+
+
+def create_employee(employee_list):
+    employee1 = employee.Employee(1001, "Otavio Barbosa", "Hourly", 10, 0, 0, 222333)
+    employee_list.append(employee1)
+    for obj in employee_list:
+        print(obj)
+    return employee_list
+
+def create_item():
+    print("Test")
+
+
+def make_purchase():
+    print("Test")
+
+
+def summary():
+    print("Test")
 
 
 if __name__ == "__main__":
